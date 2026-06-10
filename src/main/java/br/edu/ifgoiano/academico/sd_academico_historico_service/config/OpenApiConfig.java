30007d26a6a2417652a7b2f92a1a0b990c3592c9
LuisHRodrigues <luishenriquebr505@gmail.com>
2026-06-10 19:34:56 -0300

    aplicar as configs corretas da classe OpenApiConfig

--- src/main/java/br/edu/ifgoiano/academico/sd_academico_historico_service/config/OpenApiConfig.java
+++ src/main/java/br/edu/ifgoiano/academico/sd_academico_historico_service/config/OpenApiConfig.java
@@ -2,24 +2,31 @@ package br.edu.ifgoiano.academico.sd_academico_historico_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * Configuração do OpenAPI/Swagger.
  *
  * Após subir o serviço, a documentação interativa fica disponível em:
- *   - Swagger UI:   http://localhost:8086/swagger-ui.html
- *   - OpenAPI JSON: http://localhost:8086/v3/api-docs
+ * - Swagger UI: http://localhost:8086/swagger-ui.html
+ * - OpenAPI JSON: http://localhost:8086/v3/api-docs
  */
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI historicoServiceOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("Histórico Service API")
-                .description("API de consulta do histórico acadêmico gerado pelos eventos de matrícula.")
-                .version("v1"));
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("/historico").description("Via API Gateway"),
+                        new Server().url("/").description("Acesso direto ao serviço")))
+                .info(new Info()
+                        .title("Histórico Service API")
+                        .description("API de consulta do histórico acadêmico gerado pelos eventos de matrícula.")
+                        .version("v1"));
     }
 }
